a4033f90f1405fc8df9aec42ec0814419256baa6
Lazysoul <myeonginwoo@gmail.com>
2017-12-24 19:32:34 +0900

    added ch5 List Sample

--- src/main/kotlin/fp/kotlin/example/chapter05/List.kt
+++ src/main/kotlin/fp/kotlin/example/chapter05/List.kt
@@ -2,5 +2,5 @@ package fp.kotlin.example.chapter05
 
 sealed class List<out T> {
     object Nil : List<Nothing>()
-    data class Cons<out T>(val head: T, val tail: List<T>) : List<T>()
+    data class Cons<T>(val head: T, var tail: List<T>) : List<T>()
 }

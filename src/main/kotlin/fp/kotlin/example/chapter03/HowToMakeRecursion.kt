308690b7bdfcf84d1fd644d8febcd2e97c43e2b2
Lazysoul <myeonginwoo@gmail.com>
2018-03-17 21:44:19 +0900

    CH3 : main 함수를 최상단으로 이동

--- src/main/kotlin/fp/kotlin/example/chapter03/HowToMakeRecursion.kt
+++ src/main/kotlin/fp/kotlin/example/chapter03/HowToMakeRecursion.kt
@@ -1,5 +1,14 @@
 package fp.kotlin.example.chapter03
 
+fun main(args: Array<String>) {
+    println(func(5))    // 15
+}
+
+fun func(n: Int): Int = when {
+    0 >= n -> 0
+    else -> n + func(n - 1)
+}
+
 private fun helloFunc() {
     println("Hello")
     helloFunc()
@@ -13,13 +22,4 @@ private fun helloFunc(n: Int) {
             helloFunc(n - 1)
         }
     }
-}
-
-fun main(args: Array<String>) {
-    println(func(5))    // 15
-}
-
-fun func(n: Int): Int = when {
-    0 >= n -> 0
-    else -> n + func(n - 1)
 }

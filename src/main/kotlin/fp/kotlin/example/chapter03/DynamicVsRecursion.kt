761574f383899df44df0a7263ecdd6da8bb19690
Lazysoul <myeonginwoo@gmail.com>
2018-03-17 21:42:53 +0900

    CH5 : fixed dynamicVsRecursion

--- src/main/kotlin/fp/kotlin/example/chapter03/DynamicVsRecursion.kt
+++ src/main/kotlin/fp/kotlin/example/chapter03/DynamicVsRecursion.kt
@@ -3,21 +3,22 @@ package fp.kotlin.example.chapter03
 fun main(args: Array<String>) {
     println(fiboDynamic(10, IntArray(100)))    // 55
     println(fiboRecursion(10))    // 55
+    require(fiboDynamic(10, IntArray(100)) == fiboRecursion(10))
 }
 
 private fun fiboDynamic(n: Int, fibo: IntArray): Int {
+    fibo[0] = 0
     fibo[1] = 1
-    fibo[2] = 1
 
-    for (i in 0..n) {
-        fibo[i + 2] = fibo[i + 1] + fibo[i]
+    for (i in 2..n) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2]
     }
 
     return fibo[n]
 }
 
 private fun fiboRecursion(n: Int): Int = when (n) {
+    0 -> 0
     1 -> 1
-    2 -> 1
     else -> fiboRecursion(n - 2) + fiboRecursion(n - 1)
 }

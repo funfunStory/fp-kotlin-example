d19c6807f528d8e4f549b218ff818824331e24f3
Lazysoul <myeonginwoo@gmail.com>
2018-05-14 22:42:14 +0900

    CH5 : 연습문제 5-24 추가.

---
+++ src/main/kotlin/fp/kotlin/example/chapter05/exercise/5-24-printFunList.kt
@@ -0,0 +1,20 @@
+package fp.kotlin.example.chapter05.exercise
+
+import fp.kotlin.example.chapter05.FunList
+import fp.kotlin.example.chapter05.funListOf
+
+/**
+ *
+ * 연습문제 5-24
+ *
+ * FunList에 printFunList 함수를 추가해보자.
+ *
+ * 힌트: 함수의 선언 타입은 아래와 같다.
+ *
+ */
+
+fun main(args: Array<String>) {
+    require(funListOf(1, 2, 3, 4, 5).printlnFunList() == "[1, 2, 3, 4, 5]")
+}
+
+tailrec fun <T> FunList<T>.printlnFunList(acc: String = ""): String = TODO()

9044282b795eb0377e8dd2a46582347a84f85a5c
Lazysoul <myeonginwoo@gmail.com>
2018-05-20 21:55:06 +0900

    CH5 : 4장 실전응용 코드 추가.

---
+++ src/main/kotlin/fp/kotlin/example/chapter04/CallbackListener.kt
@@ -0,0 +1,49 @@
+package fp.kotlin.example.chapter04
+
+fun main(args: Array<String>) {
+
+}
+
+interface SelectListener {
+    fun onSelectedItem(item: Item)
+}
+
+// 뷰 클래스
+class SomeViewClass() : SelectListener {
+    val adatper = ItemAdatper(this)
+    // . . .
+    override fun onSelectedItem(item: Item) {
+        // 아이템 소비
+    }
+    // . . .
+}
+
+data class Item(val name: String, val desc: String)
+
+// 아이템들을 관리하는 어댑터
+class ItemAdatper(private val selectListener: SelectListener) {
+
+    private val items: List<Item> = listOf(Item("Kotlin", "Kotlin desc"),
+        Item("FP", "FP desc"))
+
+    private val view: View = TODO()
+
+    // . . .
+    // 아이템을 그리는 함수
+    fun draw(position: Int) {
+        val item = items[position]
+        // . . .
+        // 아이템이 클릭되면 콜백리스너를 통해 뷰로 아이템을 전달
+        view.setOnClickListener {
+            selectListener.onSelectedItem(item)
+        }
+        // . . .
+    }
+}
+
+//
+class View {
+    fun setOnClickListener(function: (View) -> Unit) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}

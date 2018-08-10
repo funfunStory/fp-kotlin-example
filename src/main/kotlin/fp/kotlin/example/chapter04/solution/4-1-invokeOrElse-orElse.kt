6d5361b8b357edb492297d5a17b89a6471d6ef7a
goinhacker <goinhacker@gmail.com>
2018-08-11 01:05:16 +0900

    4-1 문제 난이도 조정 및 설명 보충

--- src/main/kotlin/fp/kotlin/example/chapter04/solution/4-1-invokeOrElse-orElse.kt
+++ src/main/kotlin/fp/kotlin/example/chapter04/solution/4-1-invokeOrElse-orElse.kt
@@ -3,26 +3,14 @@ package fp.kotlin.example.chapter04.solution
 /**
  * 연습문제 4-1
  *
- * 위에서 구현한 PartialFunction 클래스에 ``invokeOrElse``함수와 ``orElse`` 함수를 추가해보자. 각  함수의 프로토타입은 아래와 같다.
- * ``invokeOrElse`` 함수는 입력값이 조건에 맞지 않을때  결과값을 설정할 수 있다.
- * ``orElse`` 함수는 입력값의 조건이 맞지 않을때 수행할 함수를 설정할 수 있다.
- * 작성된 각 함수를 활용해서 isEven 예제에서 홀수인 경우도 출력하도록 테스트하라.
+ *  위에서 구현한 ``PartialFunction`` 클래스에 ``invokeOrElse``함수와 ``orElse`` 함수를 추가해보자. 각  함수의 프로토타입은 아래와 같다.
  *
- * 힌트: 함수의 선언 타입은 아래와 같다.
  * fun invokeOrElse(p: P, default: R): R
  * fun orElse(that: PartialFunction<P, R>): PartialFunction<P, R>
+ * ``invokeOrElse`` 함수는 입력 값 ``p``가 조건에 맞지 않을때 기본값 ``default``를 반환한다.
+ * ``orElse`` 함수는 ``PartialFunction``의 입력값 ``p``가 조건에 맞으면 ``PartialFunction``을 그대로(this) 반환하고,
+ * 조건에 맞지 않으면 ``that``을 반환한다.
  */
-
-fun main(args: Array<String>) {
-    val condition: (Int) -> Boolean = { 0 == it.rem(2) }
-    val body: (Int) -> String = { "$it is even" }
-
-    val isEven = body.toPartialFunction(condition)
-    val isOdd = { i: Int -> "$i is odd" }.toPartialFunction{ !condition(it) }
-
-    println(listOf(1, 2, 3).map( isEven orElse isOdd ))    // [1 is odd, 2 is even, 3 is odd]
-    println(listOf(1, 2, 3).map { isEven.invokeOrElse(it, "$it is odd") })    // [1 is odd, 2 is even, 3 is odd]
-}
 
 class PartialFunction<P, R>(
         private val condition: (P) -> Boolean,
@@ -40,15 +28,28 @@ class PartialFunction<P, R>(
 
     fun invokeOrElse(p: P, default: R): R = if (isDefinedAt(p)) invoke(p) else default
 
-    infix fun orElse(that: PartialFunction<P, R>): PartialFunction<P, R> =
-            PartialFunction({ this.isDefinedAt(it) || that.isDefinedAt(it) }) {
-                when {
-                    this.isDefinedAt(it) -> this(it)
-                    that.isDefinedAt(it) -> that(it)
-                    else -> throw IllegalArgumentException("$it isn't defined")
-                }
-            }
+    fun orElse(that: PartialFunction<P, R>): PartialFunction<P, R> =
+            PartialFunction({ it: P -> this.isDefinedAt(it) || that.isDefinedAt(it) },
+                    { it: P ->
+                        when {
+                            this.isDefinedAt(it) -> this(it)
+                            that.isDefinedAt(it) -> that(it)
+                            else -> throw IllegalArgumentException("$it isn't defined")
+                        }
+                    }
+            )
 }
 
 fun <P, R> ((P) -> R).toPartialFunction(definedAt: (P) -> Boolean)
         : PartialFunction<P, R> = PartialFunction(definedAt, this)
+
+fun main(args: Array<String>) {
+    val condition: (Int) -> Boolean = { 0 == it.rem(2) }
+    val body: (Int) -> String = { "$it is even" }
+
+    val isEven = body.toPartialFunction(condition)
+    val isOdd = { i: Int -> "$i is odd" }.toPartialFunction{ !condition(it) }
+
+    println(listOf(1, 2, 3).map { isEven.invokeOrElse(it, "$it is odd") })    // [1 is odd, 2 is even, 3 is odd]
+    println(listOf(1, 2, 3).map { isEven.orElse(isOdd) })    // [1 is odd, 2 is even, 3 is odd]
+}

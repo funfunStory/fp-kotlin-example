b6a521ab9f2ce53b0363b8346c6be721adb54bb2
lazysou <myeonginwoo@gmail.com>
2018-10-14 23:59:12 +0900

    fixed x -> a, b

--- src/main/kotlin/fp/kotlin/example/chapter07/Function.kt
+++ src/main/kotlin/fp/kotlin/example/chapter07/Function.kt
@@ -3,8 +3,8 @@ package fp.kotlin.example.chapter07
 import fp.kotlin.example.chapter04.compose
 
 fun main(args: Array<String>) {
-    val f = { x: Int -> x + 1 }
-    val g = { x: Int -> x * 2 }
+    val f = { a: Int -> a + 1 }
+    val g = { b: Int -> b * 2 }
 
     val fg = UnaryFunction(g).fmap(f)
     println(fg.invoke(5))   // 11
@@ -16,10 +16,10 @@ fun main(args: Array<String>) {
 
 data class UnaryFunction<in T, out R>(val g: (T) -> R) : Functor<R> {
 
-//    override fun <R2> fmap(f: (R) -> R2): UnaryFunction<T, R2> {
-//        return UnaryFunction { x: T -> f(g(x)) }
-//    }
-//    override fun <R2> fmap(f: (R) -> R2) = UnaryFunction { x: T -> f(g(x)) }
+    //    override fun <R2> fmap(f: (R) -> R2): UnaryFunction<T, R2> {
+    //        return UnaryFunction { x: T -> f(g(x)) }
+    //    }
+    //    override fun <R2> fmap(f: (R) -> R2) = UnaryFunction { x: T -> f(g(x)) }
     override fun <R2> fmap(f: (R) -> R2) = UnaryFunction { x: T -> (f compose g)(x) }
 
     fun invoke(input: T): R = g(input)

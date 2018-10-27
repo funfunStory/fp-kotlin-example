8c691263fbcf5d32a9fa7c48f87985175cdee403
lazysou <myeonginwoo@gmail.com>
2018-10-28 00:09:00 +0900

    연습문제 4-8 힌트 추가.

--- src/main/kotlin/fp/kotlin/example/chapter04/exercise/4-8-takeWhile.kt
+++ src/main/kotlin/fp/kotlin/example/chapter04/exercise/4-8-takeWhile.kt
@@ -7,6 +7,8 @@ import fp.kotlin.example.tail
  * 연습문제 4-8
  *
  * 연습문제 4-7에서 작성한 ``takeWhile``를 수정하여, 무한대를 입력받을 수 있는 ``takeWhile``을 tailrec으로 작성해보자.
+ *
+ * 힌트 : ``generateSequence(1) { it + 1 }`` 는 seed값 1을 기준으로 값이 1씩 무한대로 증가하는 무한대의 리스트를 표현한다.
  */
 
 fun main(args: Array<String>) {

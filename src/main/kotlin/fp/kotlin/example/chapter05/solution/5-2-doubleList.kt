d9677a833dd56b18a343e9e2aa578ac81b14ff72
lazysou <myeonginwoo@gmail.com>
2018-10-28 00:19:22 +0900

    연습문제 5-2 설명추가.

--- src/main/kotlin/fp/kotlin/example/chapter05/solution/5-2-doubleList.kt
+++ src/main/kotlin/fp/kotlin/example/chapter05/solution/5-2-doubleList.kt
@@ -6,7 +6,7 @@ import fp.kotlin.example.chapter05.FunList.Cons
 /**
  * 연습문제 5-2
  *
- * 구현한 List를 사용해 (1.0, 2.0, 3.0, 4.0, 5.0) doubleList를 생성하자.
+ * 구현한 List를 사용해 (1.0, 2.0, 3.0, 4.0, 5.0)를 갖는 doubleList를 생성하자.
  *
  */
 

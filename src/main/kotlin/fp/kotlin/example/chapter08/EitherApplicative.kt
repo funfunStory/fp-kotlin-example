34dc166bb57d3ec4d14cbadd8a576f2171f82d44
goinhacker <goinhacker@gmail.com>
2018-11-17 23:46:48 +0900

    8.4절 Either 애플리케이티브 펑터 본문 예제 추가

---
+++ src/main/kotlin/fp/kotlin/example/chapter08/EitherApplicative.kt
@@ -0,0 +1,57 @@
+package fp.kotlin.example.chapter08
+
+import fp.kotlin.example.chapter07.Functor
+
+fun main(args: Array<String>) {
+    // fmap test
+    println(Right(10).fmap { it * 2 })  // Right(20)
+    println(Left("error").fmap { x: String -> "$x log" })   // Left(error)
+
+    // pure test
+    println(Either.pure(10))    // Right(10)
+    println(Either.pure({ x: Int -> x * 2 }))   // Right((kotlin.Int) -> kotlin.Int)
+
+    // apply test
+    println(Either.pure({ x: Int -> x * 2 }) apply Left("error"))   // Left(error)
+    println(Either.pure({ x: Int -> x * 2 }) apply Right(10))   // Right(20)
+    println(Either.pure({ x: Int, y: Int -> x * y }.curried())
+        apply Left("error")
+        apply Right(10)
+    )   // Left(error)
+    println(Either.pure({ x: Int, y: Int -> x * y }.curried())
+            apply Right(10)
+            apply Right(20)
+    )   // Right(200)
+}
+
+sealed class Either<out L, out R> : Functor<R> {
+
+    abstract override fun <R2> fmap(f: (R) -> R2): Either<L, R2>
+
+    companion object
+}
+
+data class Left<out L>(val value: L) : Either<L, kotlin.Nothing>() {
+
+    override fun toString(): String = "Left($value)"
+
+    override fun <R2> fmap(f: (kotlin.Nothing) -> R2): Either<L, R2> = this
+
+}
+
+data class Right<out R>(val value: R) : Either<kotlin.Nothing, R>() {
+
+    override fun toString(): String = "Right($value)"
+
+    override fun <R2> fmap(f: (R) -> R2): Either<kotlin.Nothing, R2> = Right(f(value))
+}
+
+fun <A> Either.Companion.pure(value: A) = Right(value)
+
+infix fun <L, A, B> Either<L, (A) -> B>.apply(f: Either<L, A>): Either<L, B> = when (this) {
+    is Left -> this
+    is Right -> f.fmap(value)
+}
+
+private fun <P1, P2, R> ((P1, P2) -> R).curried(): (P1) -> (P2) -> R =
+        { p1: P1 -> { p2: P2 -> this(p1, p2) } }
--- src/main/kotlin/fp/kotlin/example/chapter08/MaybeApplicative.kt
+++ src/main/kotlin/fp/kotlin/example/chapter08/MaybeApplicative.kt
@@ -4,30 +4,30 @@ import fp.kotlin.example.chapter07.Functor
 
 fun main(args: Array<String>) {
     // fmap test
-    println(Just(10).fmap { it + 10 })   // AJust(20)
-    println(Nothing.fmap { it: Int -> it + 10 })  // ANothing
+    println(Just(10).fmap { it + 10 })   // Just(20)
+    println(Nothing.fmap { it: Int -> it + 10 })  // Nothing
 
     // pure test
-    println(Maybe.pure(10))  // AJust(10)
-    println(Maybe.pure({ x: Int -> x * 2 }))  // AJust((kotlin.Int) -> kotlin.Int)
+    println(Maybe.pure(10))  // Just(10)
+    println(Maybe.pure({ x: Int -> x * 2 }))  // Just((kotlin.Int) -> kotlin.Int)
 
     // apply test
-    println(Maybe.pure({ x: Int -> x * 2 }) apply Just(10))  // AJust(20)
-    println(Maybe.pure({ x: Int -> x * 2 }) apply Nothing)   // ANothing
+    println(Maybe.pure({ x: Int -> x * 2 }) apply Just(10))  // Just(20)
+    println(Maybe.pure({ x: Int -> x * 2 }) apply Nothing)   // Nothing
 
     // applicative style programming test
-//    println(AMaybe.pure({ x: Int, y: Int -> x * y}) apply AJust(10) apply AJust(20))  // compile error
+//    println(AMaybe.pure({ x: Int, y: Int -> x * y}) apply Just(10) apply Just(20))  // compile error
 
     println(Maybe.pure({ x: Int, y: Int -> x * y }.curried())
             apply Just(10)
             apply Just(20)
-    )   // AJust(200)
+    )   // Just(200)
 
     println(Maybe.pure({ x: Int, y: Int, z: Int -> x * y + z }.curried())
             apply Just(10)
             apply Just(20)
             apply Just(30)
-    )   // AJust(230)
+    )   // Just(230)
 }
 
 sealed class Maybe<out A> : Functor<A> {
@@ -41,14 +41,14 @@ sealed class Maybe<out A> : Functor<A> {
 
 data class Just<out A>(val value: A) : Maybe<A>() {
 
-    override fun toString(): String = "AJust($value)"
+    override fun toString(): String = "Just($value)"
 
     override fun <B> fmap(f: (A) -> B): Maybe<B> = Just(f(value))
 }
 
 object Nothing : Maybe<kotlin.Nothing>() {
 
-    override fun toString(): String = "ANothing"
+    override fun toString(): String = "Nothing"
 
     override fun <B> fmap(f: (kotlin.Nothing) -> B): Maybe<B> = Nothing
 }

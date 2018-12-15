270ef25c3cd12cebfca284879d6124b0ba52334a
goinhacker <goinhacker@gmail.com>
2018-12-15 22:19:51 +0900

    Maybe 모노이드 예제 추가

---
+++ src/main/kotlin/fp/kotlin/example/chapter09/Monoid.kt
@@ -0,0 +1,13 @@
+package fp.kotlin.example.chapter09
+
+import fp.kotlin.example.chapter05.FunList
+import fp.kotlin.example.chapter05.foldRight
+
+interface Monoid<T> {
+
+    fun mempty(): T
+
+    fun mappend(m1: T, m2: T): T
+}
+
+fun <T> Monoid<T>.mconcat(list: FunList<T>): T = list.foldRight(mempty(), ::mappend)
---
+++ src/main/kotlin/fp/kotlin/example/chapter09/SumMonoid.kt
@@ -0,0 +1,24 @@
+package fp.kotlin.example.chapter09
+
+import fp.kotlin.example.chapter05.funListOf
+
+fun main(args: Array<String>) {
+    val x = 1
+    val y = 2
+    val z = 3
+
+    SumMonoid().run {
+        println(mappend(mempty(), x) == x)  // true
+        println(mappend(x, mempty()) == x)  // true
+        println(mappend(mappend(x, y), z) == mappend(x, mappend(y, z)))  // true
+    }
+
+    println(SumMonoid().mconcat(funListOf(1, 2, 3, 4, 5)))      // 15
+}
+
+class SumMonoid : Monoid<Int> {
+
+    override fun mempty(): Int = 0
+
+    override fun mappend(m1: Int, m2: Int): Int = m1 + m2
+}

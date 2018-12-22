7f56da95670e93276bcdd50f6f7ea0c50f22dec6
lazysou <myeonginwoo@gmail.com>
2018-12-23 01:26:53 +0900

    연습문제 9-10 추가.

---
+++ src/main/kotlin/fp/kotlin/example/chapter09/solution/9_10_FunListMonoidWithFoldable.kt
@@ -0,0 +1,31 @@
+package fp.kotlin.example.chapter09.solution
+
+import fp.kotlin.example.chapter09.Foldable
+import fp.kotlin.example.chapter09.SumMonoid
+
+/**
+ * 연습문제 9-10
+ *
+ * FunList 모노이드를 Foldable 타입클래스의 인스턴스로 만들어서 ``foldLeft`` 함수를 작성하고, ``foldMap`` 함수를 테스트해보자.
+ *
+ */
+
+fun main(args: Array<String>) {
+
+    val list1 = Cons(1, Cons(2, Cons(3, Cons(4, Nil))))
+
+    require(list1.foldLeft(0) { acc, value -> acc + value } == 10)
+
+    require(list1.foldMap({ x -> x + 1 }, SumMonoid()) == 14)
+    require(list1.foldMap({ x -> x * 2 }, SumMonoid()) == 20)
+}
+
+sealed class FunList<out T> : Foldable<T>
+
+object Nil : FunList<Nothing>() {
+    override fun <B> foldLeft(acc: B, f: (B, Nothing) -> B): B = acc
+}
+
+data class Cons<out T>(val head: T, val tail: FunList<T>) : FunList<T>() {
+    override fun <B> foldLeft(acc: B, f: (B, T) -> B): B = tail.foldLeft(f(acc, head), f)
+}

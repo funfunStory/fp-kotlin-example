fb7dbff0192453ac4f256e8ddc5bebcf6daf9153
lazysou <myeonginwoo@gmail.com>
2018-12-23 00:14:43 +0900

    연습문제 9-3, 4 추가.

---
+++ src/main/kotlin/fp/kotlin/example/chapter09/exercise/9_4_AllMonoid_MonoidLaw.kt
@@ -0,0 +1,23 @@
+package fp.kotlin.example.chapter09.exercise
+
+
+/**
+ *
+ * 연습문제 9-4
+ *
+ * All 모노이드가 모노이드의 법칙을 만족하는지 테스트해보자.
+ *®
+ */
+
+
+fun main(args: Array<String>) {
+    val x = true
+    val y = false
+    val z = true
+
+    AllMonoid().run {
+        require(mappend(x, mempty()) == x)
+        require(mappend(mempty(), x) == x)
+        require(mappend(x, mappend(y, z)) == mappend(mappend(x, y), z))
+    }
+}

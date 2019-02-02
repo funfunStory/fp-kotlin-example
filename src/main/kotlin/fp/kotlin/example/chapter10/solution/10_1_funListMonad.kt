e7a267fb236eb4b701b10aeb48b3ecf9fbf8a021
lazysoul <myeonginwoo@gmail.com>
2019-02-02 11:02:27 -0500

    연습문제 10-1, 10-2

--- src/main/kotlin/fp/kotlin/example/chapter10/solution/10_1_funListMonad.kt
+++ src/main/kotlin/fp/kotlin/example/chapter10/solution/10_1_funListMonad.kt
@@ -8,45 +8,53 @@ import fp.kotlin.example.chapter10.Monad
  *
  * ``Monad`` 타입클래스의 인스턴스인 ``FunList``를 작성하여 리스트 모나드를 만들어보자.
  *
- * 힌트 : append 함수를 사용하자.
+ * 힌트 : mappend 함수를 사용하자.
  */
 
 fun main() {
-    val funList = Cons(1,Cons(2, Cons(3, Nil)))
-    val result = funList.flatMap { Cons(it, Cons(it*2, Cons(it*3, Nil))) }
+    val funList = Cons(1, Cons(2, Cons(3, Nil)))
+    val result = funList.flatMap { Cons(it, Cons(it * 2, Cons(it * 3, Nil))) }
 
     require(result == Cons(1, Cons(2, Cons(3, Cons(2, Cons(4, Cons(6, Cons(3, Cons(6, Cons(9, Nil))))))))))
 
     val nilList = Nil
-    val nilResult = nilList.flatMap { x: Int -> Cons(x, Cons(x*2, Cons(x*3, Nil))) }
+    val nilResult = nilList.flatMap { x: Int -> Cons(x, Cons(x * 2, Cons(x * 3, Nil))) }
 
     require(nilResult == Nil)
 }
 
-sealed class FunList <out A>: Monad<A> {
+sealed class FunList<out A> : Monad<A> {
 
-    override infix fun <V> pure(value: V): Monad<V> = Cons(value, Nil)
+    companion object {
+        fun <V> pure(value: V): FunList<V> = Cons(0, Nil).pure(value) as FunList<V>
+    }
 
-    override infix fun <B> flatMap(f: (A) -> Monad<B>): Monad<B> = when(this) {
+    override infix fun <V> pure(value: V): Monad<V> = when (this) {
+        Nil -> Nil
+        is Cons -> Cons(value, Nil)
+    }
+
+    override infix fun <B> flatMap(f: (A) -> Monad<B>): Monad<B> = when (this) {
         Nil -> Nil
         is Cons -> f(head) as FunList<B> mappend tail.flatMap(f) as FunList<B>
     }
 
-    infix fun <A>FunList<A>.mappend(other: FunList<A>): FunList<A>  = when{
+    infix fun <A> FunList<A>.mappend(other: FunList<A>): FunList<A> = when {
         this is Nil -> other
         other is Nil -> this
         this is Cons && other is Cons -> Cons(this.head, this.tail.mappend(other))
         else -> Nil
     }
 
-    infix fun <A, B> FunList<(A) -> B>.apply(f: FunList<A>): FunList<B> = when (this) {
-        Nil -> Nil
-        is Cons -> Cons(f.fmap(head) as B, tail.apply(f))
-    }
-
     infix fun <B> leadTo(m: FunList<B>): FunList<B> = flatMap { m } as FunList
 }
 
+infix fun <A, B> FunList<(A) -> B>.ap(f: FunList<A>): FunList<B> = when (this) {
+    Nil -> Nil
+    is Cons -> f.fmap(head) as FunList<B> mappend tail.ap(f)
+}
+
+
 data class Cons<out A>(val head: A, val tail: FunList<A>) : FunList<A>()
 
-object Nil: FunList<Nothing>()
+object Nil : FunList<Nothing>()

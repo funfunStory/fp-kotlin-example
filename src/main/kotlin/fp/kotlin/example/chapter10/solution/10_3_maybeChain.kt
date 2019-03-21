88db84deb2abe2ba715b8bdfb2d42ffa29ee205a
myeonginwoo <myeonginwoo@gmail.com>
2019-03-20 22:48:59 -0400

    10-3 코드 제거 & 힌트 추가.

--- src/main/kotlin/fp/kotlin/example/chapter10/solution/10_3_maybeChain.kt
+++ src/main/kotlin/fp/kotlin/example/chapter10/solution/10_3_maybeChain.kt
@@ -9,6 +9,7 @@ import fp.kotlin.example.chapter10.Nothing
  *
  * 아래와 같이 중첩된 클래스 구조 중간에 nullable하지 않은 프로퍼티 ``D3``가 있다고 가정했을때, 본문 예제와 동일한 기능의 ``getValueOfD4`` 함수를 작성하고 테스트해보자.
  *
+ * 힌트 : d에 접근할 때  fmap을 사용해 값에 접근 하고 함수 체이닝을 이어 갈 수 있도록 getValueOfD4_2 함수도 작성하자.
  */
 
 fun main() {
@@ -41,9 +42,9 @@ class D4(val value: Maybe<String>)
 
 fun getValueOfD4(a: A4): Maybe<String> = a.b
         .flatMap { it.c }
-        .flatMap { it.d.value } as Maybe<String>
+        .flatMap { it.d.value }
 
 fun getValueOfD4_2(a: A4): Maybe<String> = a.b
         .flatMap { it.c }
         .fmap { it.d }
-        .flatMap { it.value } as Maybe<String>
+        .flatMap { it.value }

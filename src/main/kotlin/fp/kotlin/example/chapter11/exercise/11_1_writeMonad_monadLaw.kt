f7ff8ef3db98f69255378b0df5ff0decabfb751d
myeonginwoo <myeonginwoo@gmail.com>
2019-03-23 22:13:26 -0400

    연습문제 11-1 추가 및 pure 추가.

---
+++ src/main/kotlin/fp/kotlin/example/chapter11/exercise/11_1_writeMonad_monadLaw.kt
@@ -0,0 +1,24 @@
+package fp.kotlin.example.chapter11.exercise
+
+import fp.kotlin.example.chapter11.logging.WriterMonad
+
+/**
+ *
+ * 연습문제 11-1
+ *
+ * WriterMonad가 모나드의 법칙을 만족하는지 확인해보자.
+ *
+ */
+
+fun main() {
+    val f: (Int) -> WriterMonad<Int> = { x: Int -> WriterMonad.pure(x) }
+    val value = 3
+
+    require(WriterMonad.pure(value) flatMap f == f(value))
+
+    val m = WriterMonad.pure(value)
+    require(m flatMap { WriterMonad.pure(it) } == m)
+
+    val g = {x : Int -> WriterMonad.pure(value) }
+    require((m flatMap f) flatMap g == m flatMap { x :Int -> f(x) flatMap g })
+}

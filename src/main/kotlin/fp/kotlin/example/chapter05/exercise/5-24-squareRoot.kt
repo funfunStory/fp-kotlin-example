5afee4a71a2a98166f307959ffda1a6c3fe344fd
myeonginwoo <myeonginwoo@gmail.com>
2019-03-29 17:57:03 -0400

    연습문제 5-24 문제 수정.

--- src/main/kotlin/fp/kotlin/example/chapter05/exercise/5-24-squareRoot.kt
+++ src/main/kotlin/fp/kotlin/example/chapter05/exercise/5-24-squareRoot.kt
@@ -6,12 +6,12 @@ package fp.kotlin.example.chapter05.exercise
  *
  * 모든 자연수의 제곱근의 합이 1000을 넘으려면 몇개의 자연수가 필요한지 계산하는 함수를 작성해보자.
  *
- * 힌트: 함수는 꼬리재귀로 작성하며, 파라미터는 스스로 정의해서(있을 수도 없을수도..) 문제를 풀어보자.
+ * 힌트: 함수는 꼬리재귀로 작성하자.
  *
  */
 
 fun main(args: Array<String>) {
-    require(squareRoot() == 10)
+//    require(squareRoot() == 10)
 }
 
-tailrec fun squareRoot(): Int = TODO()
+//tailrec fun squareRoot(something): Int = TODO()

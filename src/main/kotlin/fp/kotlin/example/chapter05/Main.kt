4998144a760551122b7cc4648549602804d29fc6
goinhacker <goinhacker@gmail.com>
2019-07-29 22:36:53 +0900

    연산순서 오류 수정

--- src/main/kotlin/fp/kotlin/example/chapter05/Main.kt
+++ src/main/kotlin/fp/kotlin/example/chapter05/Main.kt
@@ -23,9 +23,9 @@ fun main() {
 
     printFunList(intList.mapByFoldLeft { it + 3 })  // [4, 5, 6]
 
-    val intList2 = funListOf(1, 3, 10)
-    println(intList2.foldRight(0) { x, acc -> x - acc })    // 8
-    println(intList2.foldLeft(0) { acc, x -> x - acc })    // 8
+    val intList2 = funListOf(1, 3, 10, 14)
+    println(intList2.foldRight(0) { x, acc -> x - acc })    // -6
+    println(intList2.foldLeft(0) { acc, x -> x - acc })    // 6
 
     printFunList(intList.mapByFoldRight { it + 3 })  // [4, 5, 6]
 

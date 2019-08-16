9289118681c90f33137ecbca9fcabbc689f6d39e
goinhacker <goinhacker@gmail.com>
2019-08-16 23:52:17 +0900

    연습문제 문장 수정

--- src/main/kotlin/fp/kotlin/example/chapter09/solution/9_5_AnyMonoid_mconcat.kt
+++ src/main/kotlin/fp/kotlin/example/chapter09/solution/9_5_AnyMonoid_mconcat.kt
@@ -7,10 +7,9 @@ import fp.kotlin.example.chapter09.mconcat
  *
  * 연습문제 9-5
  *
- * Any 모노이드의 ``mconcat`` 함수를 테스트 해보자. 예를들어 [true, true, true], [false, false, false], [ture, false, true] 인 경우
- *
+ * ``Any`` 모노이드의 ``mconcat`` 함수를 테스트해 보자. 예를 들어 입력이 [true, true, true], [false, false, false],
+ * [ture, false, true] 인 경우 결과는 어떤가?
  */
-
 fun main(args: Array<String>) {
     val x = funListOf(true, true, true)
     val y = funListOf(false, false, false)

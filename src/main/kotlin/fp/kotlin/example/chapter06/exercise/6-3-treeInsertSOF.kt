d14e279ba81e2d3315871277ac1b1944b414a5ce
goinhacker <goinhacker@gmail.com>
2019-09-12 03:25:17 +0900

    6장 연습문제 수정

--- src/main/kotlin/fp/kotlin/example/chapter06/exercise/6-3-treeInsertSOF.kt
+++ src/main/kotlin/fp/kotlin/example/chapter06/exercise/6-3-treeInsertSOF.kt
@@ -6,7 +6,7 @@ package fp.kotlin.example.chapter06.exercise
  *
  * 연습문제 6-2 에서 작성한 insert 코드를 100000만번 이상 연속해서 insert 해 보자.
  *
- * 힌트 : 테스트하는 디바이스마다 다르겠지만 StackOverflowError 가 날 때까지 해보자.
+ * 힌트 : 테스트하는 디바이스마다 오류가 발생하는 시기는 다르겠지만 StackOverflowError 가 날 때까지 해보자.
  *
  */
 

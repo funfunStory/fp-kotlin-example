7d44be55e4d40ec8e8f7f7d3f4f19fe1cfd836a3
goinhacker <goinhacker@gmail.com>
2019-09-16 01:18:42 +0900

    3장 연습문제 수정

--- src/main/kotlin/fp/kotlin/example/chapter03/solution/3-8-quicksort.kt
+++ src/main/kotlin/fp/kotlin/example/chapter03/solution/3-8-quicksort.kt
@@ -6,11 +6,11 @@ import fp.kotlin.example.tail
 /**
  * 연습문제 3-8
  *
- * quicksort 함수를 작성해보자.
+ * 퀵정렬 알고리즘의 ``quicksort`` 함수를 작성해 보자.
  *
  * 힌트1: 퀵정렬 알고리즘의 동작 방식은 아래 위키를 참고하자.
  * https://en.wikipedia.org/wiki/Quicksort
- * 힌트2: 리스트를 분할하기 위한 함수 partition을 활용해보자.
+ * 힌트2: 리스트를 분할하기 위해 함수 ``partition``을 활용하자.
  */
 
 fun main(args: Array<String>) {
